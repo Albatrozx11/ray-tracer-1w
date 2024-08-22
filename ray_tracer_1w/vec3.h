ac3f8c0e975c4c4900966025b01dfb712a592af9
AdithyanA <adithyanayithala@gmail.com>
2024-08-22 22:22:53 +0530

    added vec3.h

--- ray_tracer_1w/vec3.h
+++ ray_tracer_1w/vec3.h
@@ -15,11 +15,11 @@ public:
 	double y() const { return e[1]; }
 	double z() const { return e[2]; }
 
-	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); 
+	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 	double operator[](int i) const { return e[i]; }
 	double& operator[](int i) { return e[i]; }
-
-	vec3 operator+=(const vec3& v) {
+	
+	vec3 operator+=(const vec3 & v) {
 		e[0] += v.e[0];
 		e[1] += v.e[1];
 		e[2] += v.e[2];
@@ -37,7 +37,7 @@ public:
 		return *this *= t;
 	}
 
-	double length(const vec3& v) {
+	double length() const {
 		return std::sqrt(length_squared());
 	}
 
@@ -47,6 +47,44 @@ public:
 
 };
 
-	std::ostream& operator<<(std::ostream stream, const vec3& other) {
+inline std::ostream& operator<<(std::ostream stream, const vec3& other) {
+	return std::cout << other.e[0] << "," << other.e[1] << "," << other.e[2] << "\n";
+}
 
-	}
+inline vec3 operator+(const vec3& v, const vec3& u) {
+	return vec3(v.e[0] + u.e[0], v.e[1] + u.e[1], v.e[2] + u.e[2]);
+}
+
+inline vec3 operator-(const vec3& v, const vec3& u) {
+	return vec3(v.e[0] - u.e[0], v.e[1] - u.e[1], v.e[2] - u.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, const vec3& u) {
+	return vec3(v.e[0] * u.e[0], v.e[1] * u.e[1], v.e[2] * u.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+	return vec3(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+	return v * t;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+	return v * (1/t);
+}
+
+inline double dot(const vec3& v, const vec3& u) {
+	return (v.e[0] * u.e[0] + v.e[1] * u.e[1] + v.e[2] * u.e[2]);
+}
+
+inline vec3 cross(const vec3& v, const vec3& u) {
+	return vec3(v.e[1] * u.e[2] - v.e[2] * u.e[1] ,
+				v.e[2] * u.e[0] - v.e[0] * u.e[2] ,
+				v.e[0] * u.e[1] - v.e[1] * u.e[0]);
+}
+
+inline vec3 unit_vector(const vec3& v) {
+	return v/v.length();
+}
