201c1175518f7cb4f1f460a955c479a8920c666b
AdithyanA <adithyanayithala@gmail.com>
2024-09-21 22:12:08 +0530

    feat: rendered first sphere

--- ray_tracer_1w/main.cpp
+++ ray_tracer_1w/main.cpp
@@ -3,10 +3,25 @@
 #include "color.h"
 #include "ray.h"
 
+
+
+bool hit_sphere(const point3& center,double radius, ray r) {
+	vec3 positon_vector = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), positon_vector);
+	auto c = dot(positon_vector, positon_vector) - (radius * radius);
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+
+	if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+		return color(1, 0, 0);
+	}
 	auto ray_direction_scaled = unit_vector(r.direction());
-	auto a = 0.5 * (1 + ray_direction_scaled.y());
-	return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+	auto a = 0.5 * (1.0 + ray_direction_scaled.y());
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
